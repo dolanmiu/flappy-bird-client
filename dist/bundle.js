75fba55b57880deafbacbcfb2a34e3a52acd41da
Dolan Miu <dolan_miu@hotmail.com>
2016-12-19 22:23:33 +0000

    made background more responsive

--- dist/bundle.js
+++ dist/bundle.js
@@ -46,7 +46,7 @@ var Flappy;
     class Game extends Phaser.Game {
         constructor(elementName) {
             let element = document.getElementById(elementName);
-            super(window.innerWidth, 800, Phaser.AUTO, element.id, Flappy.State.Play, true, false);
+            super(window.innerWidth, window.innerHeight, Phaser.AUTO, element.id, Flappy.State.Play, true, false);
             window.addEventListener('resize', (myFunction) => {
                 this.scale.setGameSize(window.innerWidth, window.innerHeight);
             });
--- src/game.ts
+++ src/game.ts
@@ -3,7 +3,7 @@ namespace Flappy {
         constructor(elementName: string) {
             let element = document.getElementById(elementName);
 
-            super(window.innerWidth, 800, Phaser.AUTO, element.id, Flappy.State.Play, true, false);
+            super(window.innerWidth, window.innerHeight, Phaser.AUTO, element.id, Flappy.State.Play, true, false);
             window.addEventListener('resize', (myFunction) => {
                 this.scale.setGameSize(window.innerWidth, window.innerHeight);
             });
