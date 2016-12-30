6eeec7220a8f8f997540910ee5d99c7cb363cfb1
Dolan Miu <dolan_miu@hotmail.com>
2016-12-30 21:52:12 +0000

    added name tag to multiplayer bird

--- src/bird/multiplayer-bird.ts
+++ src/bird/multiplayer-bird.ts
@@ -1,5 +1,15 @@
 namespace Flappy {
     export class MultiplayerBird extends BaseBird {
 
+        private nameTag: Phaser.Sprite;
+
+        constructor(game: Phaser.Game, x: number, y: number, private displayName: string, params: IBirdParams) {
+            super(game, x, y, params);
+
+            this.nameTag = new Phaser.Text(game, 0, 0, displayName);
+            this.addChild(this.nameTag);
+
+            this.game.add.existing(this);
+        }
     }
 }
