b58da41a0b8f35b47f859c06311029c92a1125fb
Dolan Miu <dolan_miu@hotmail.com>
2023-09-07 14:06:23 +0100

    Use Glitch instead of Heroku

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -11,7 +11,7 @@ namespace Flappy.Global {
             if (window.location.href === 'http://127.0.0.1:8080/' || window.location.href === 'http://localhost:8080/') {
                 return 'http://localhost:9001';
             } else {
-                return 'https://flappy-bird-server.herokuapp.com';
+                return 'https://flappy-bird-server.glitch.me';
             }
         }
 
